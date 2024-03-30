8d0500fb2d94227849d8c2dc11c09c8b8c05813c
tonpm14 <uo266575@uniovi.es>
2024-03-30 10:14:30 +0100

    Markers warning

--- js/velutinas.js
+++ js/velutinas.js
@@ -1,4 +1,5 @@
 var mymap = L.map('map').setView([43.3623, -5.8448], 13);
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
